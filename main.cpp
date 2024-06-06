e3a58367db49826486ee87f57169633de22bed2f
khaakila <98585432+khaakila@users.noreply.github.com>
2024-06-06 23:15:21 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,14 +1,1 @@
-<HTML>
-<HEADE><TITLE>Dev-C++ project: Project3</TITLE></HEAD>
-<BODY BGCOLOR=#FFFFFF>
-<H2>Project: Project3</H2>
-<B>Index of files:</B>
-<HR WIDTH="80%">
-<TABLE ALIGN="CENTER" CELLSPACING=20>
-<TR><TD><B><U>Filename</U></B></TD><TD><B><U>Location</U></B></TD><TD><B><U>Size</U></B></TD></TR>
-<TR><TD><A HREF="files/TEMPLA~1_main_cpp.html">main.cpp</A></TD><TD>C:\Dev-Cpp\TEMPLA~1\main.cpp</TD><TD>6 Kb</TD></TR>
-</TABLE>
-<HR WIDTH="80%">
-<P ALIGN="CENTER"><FONT SIZE=1>Exported by <A HREF="http://www.bloodshed.net/dev">Dev-C++</A> v4.9.9.2</FONT></P>
-</BODY>
-</HTML>
+
